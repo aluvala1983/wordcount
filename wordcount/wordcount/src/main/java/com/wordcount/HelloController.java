2c7709ff9edc88aa1a4a4af1323f28a21de590a8
Mahe1983 <mahesh.aluvala@gmail.com>
2019-06-29 19:22:28 +0530

    First Commit

---
+++ wordcount/wordcount/src/main/java/com/wordcount/HelloController.java
@@ -0,0 +1,16 @@
+package com.wordcount;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/SayHello")
+	public String Hello()
+	{
+		return "<H1>Hi Mahesh, Executing from AWS... Hello All </H1>";
+	}
+
+}
+ 	
